5035e7d872eccd2233451e801193e7ae341af1bd
1uked <123551653+1uked@users.noreply.github.com>
2025-03-17 11:40:52 +0000

    Implement Forward Fill

--- lgbm_pipeline/feature_extraction.py
+++ lgbm_pipeline/feature_extraction.py
@@ -1,12 +1,19 @@
 import feature_load as loader
 import pandas as pd
+import pprint
+import numpy as np
 
-def cleanData(df: pd.DataFrame,
-               ):
+def cleanData(df: pd.DataFrame) -> pd.DataFrame:
+  clean_df = df.copy()
+  clean_df.ffill(inplace=True)
+  clean_df.fillna(value=1, inplace=True)
 
-  return 0
+  return clean_df
 
 
 
 df_raw = loader.loadTrainingData(path_pattern='../training_setA/*.psv', max_files=1000)
-print("DataFrame columns:", df_raw.columns)
+df_clean = cleanData(df_raw)
+print(df_raw.head(10))
+print(df_clean.head(10))
+print("DataFrame columns:", df_clean.columns)
