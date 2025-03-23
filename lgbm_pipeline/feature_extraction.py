34f233ec822e95f8a800910572ba980e93877d52
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-03-23 21:49:39 +0000

    fill methods

--- lgbm_pipeline/feature_extraction.py
+++ lgbm_pipeline/feature_extraction.py
@@ -1,9 +1,25 @@
+from enum import Enum
 import pandas as pd
 
-def clean_data(patients: list[pd.DataFrame]) -> list[pd.DataFrame]:
-  cleaned = []
-  for patient in patients:
-      clean_df = patient.ffill()
-      clean_df.fillna(value=1, inplace=True) # doesn't make sense for pH
-      cleaned.append(clean_df)
-  return cleaned
+class FillMethod(Enum):
+	FORWARD = 1
+	BACKWARD = 2
+	LINEAR = 3
+	MIXED = 4
+
+def clean_data(patients: list[pd.DataFrame], method=FillMethod.FORWARD) -> list[pd.DataFrame]:
+	cleaned = []
+	for patient in patients:
+		if method == FillMethod.FORWARD:
+			clean_df = patient.ffill(inplace=False)
+			clean_df.bfill(inplace=True)
+		elif method == FillMethod.BACKWARD:
+			clean_df = patient.bfill(inplace=False)
+			clean_df.ffill(inplace=True)
+		elif method == FillMethod.LINEAR:
+			clean_df = patient.interpolate(inplace=False)
+			clean_df.ffill(inplace=True)
+			clean_df.bfill(inplace=True)
+		clean_df.fillna(value=1, inplace=True) # doesn't make sense for pH
+		cleaned.append(clean_df)
+	return cleaned
