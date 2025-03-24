366be5d5c19ec78a6bd73968cdca7f7e7fc4ab34
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-03-24 02:08:43 +0000

    mixed fill possibly working

--- pipelines_le/lgbm_pipeline/feature_extraction.py
+++ pipelines_le/lgbm_pipeline/feature_extraction.py
@@ -1,25 +1,56 @@
-from enum import Enum
+from enum import Enum, auto
 import pandas as pd
 
 class FillMethod(Enum):
-	FORWARD = 1
-	BACKWARD = 2
-	LINEAR = 3
-	MIXED = 4
+	FORWARD = auto()
+	BACKWARD = auto()
+	LINEAR = auto()
 
-def clean_data(patients: list[pd.DataFrame], method=FillMethod.FORWARD) -> list[pd.DataFrame]:
-	cleaned = []
+def fill(patients: list[pd.DataFrame], method=FillMethod.FORWARD) -> list[pd.DataFrame]:
+	filled_data = []
 	for patient in patients:
+		#patient.dropna(axis="columns", how="all", inplace=True)
 		if method == FillMethod.FORWARD:
-			clean_df = patient.ffill(inplace=False)
-			clean_df.bfill(inplace=True)
+			filled_df = patient.ffill(inplace=False)
+			filled_df.bfill(inplace=True)
+			filled_data.append(filled_df)
 		elif method == FillMethod.BACKWARD:
-			clean_df = patient.bfill(inplace=False)
-			clean_df.ffill(inplace=True)
+			filled_df = patient.bfill(inplace=False)
+			filled_df.ffill(inplace=True)
+			filled_data.append(filled_df)
 		elif method == FillMethod.LINEAR:
-			clean_df = patient.interpolate(inplace=False)
-			clean_df.ffill(inplace=True)
-			clean_df.bfill(inplace=True)
-		clean_df.fillna(value=1, inplace=True) # doesn't make sense for pH
-		cleaned.append(clean_df)
-	return cleaned
+			filled_df = patient.interpolate(inplace=False)
+			filled_df.ffill(inplace=True)
+			filled_df.bfill(inplace=True)
+			filled_data.append(filled_df)
+
+	return filled_data
+
+def correlation(correlation_matrices: dict[FillMethod, pd.DataFrame], fill_method: FillMethod, feature: str) -> float:
+	return correlation_matrices[fill_method][feature]["SepsisLabel"]
+
+def mixed_fill(patients: dict[FillMethod, list[pd.DataFrame]]) -> pd.DataFrame:
+	all_patients: dict[FillMethod, pd.DataFrame] = {FillMethod.FORWARD: pd.concat(patients[FillMethod.FORWARD]),
+	                FillMethod.BACKWARD: pd.concat(patients[FillMethod.BACKWARD]),
+	                FillMethod.LINEAR: pd.concat(patients[FillMethod.LINEAR])}
+
+	all_patients_mixed: pd.DataFrame = pd.DataFrame(columns=all_patients[FillMethod.FORWARD].columns)
+
+	correlation_matrices: dict[FillMethod, pd.DataFrame] = {FillMethod.FORWARD: all_patients[FillMethod.FORWARD].corr(),
+	                        FillMethod.BACKWARD: all_patients[FillMethod.BACKWARD].corr(),
+	                        FillMethod.LINEAR: all_patients[FillMethod.LINEAR].corr()}
+
+	for feature in all_patients_mixed.columns:
+		max_corr: float = 0
+		best_method: FillMethod = FillMethod.FORWARD
+
+		for method in FillMethod:
+			corr: float = correlation(correlation_matrices, method, feature)
+			if abs(corr) > max_corr:
+				max_corr = corr
+				best_method = method
+
+		# fill that feature with the method that gave the highest correlation with SepsisLabel
+		all_patients_mixed[feature] = all_patients[best_method][feature]
+
+	return all_patients_mixed
