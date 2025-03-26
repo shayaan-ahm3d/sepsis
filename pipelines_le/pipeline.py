763077c7e78f07e61ce945b3266f2ceacfd3d28c
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-03-26 23:16:42 +0000

    concatenate dataframes

--- pipelines_le/pipeline.py
+++ pipelines_le/pipeline.py
@@ -11,7 +11,7 @@ from sklearn.metrics import fbeta_score, make_scorer, RocCurveDisplay, Confusion
 import lightgbm as lgbm
 import xgboost as xgb
 
-patients: list[pd.DataFrame] = loader.load_training_data(f"{os.getcwd()}/training_setA/*.psv")
+patients: list[pd.DataFrame] = loader.load_training_data(f"../training_set?/*.psv", max_files=None)
 # f = open("patients", "w")
 # pickle.dump(patients, f)
 
@@ -28,7 +28,8 @@ for patient in tqdm(patients, "Converting indices to timedeltas"):
 train_sepsis_patients, test_sepsis_patients = train_test_split(sepsis_patients)
 train_non_sepsis_patients, test_non_sepsis_patients = train_test_split(non_sepsis_patients)
 
-weight: float = len(train_non_sepsis_patients)/len(train_sepsis_patients)
+ratio: float = len(train_non_sepsis_patients)/len(train_sepsis_patients)
+print(f"Ratio: {ratio}")
 
 train_patients: list[pd.DataFrame] = train_sepsis_patients + train_non_sepsis_patients
 test_patients: list[pd.DataFrame] = test_sepsis_patients + test_non_sepsis_patients
@@ -57,34 +58,42 @@ train_patients_mixed = extractor.mixed_fill(train_patients, train_patients_forwa
 test_patients_mixed = extractor.mixed_fill(test_patients, test_patients_forward, test_patients_backward, test_patients_linear, fill_methods_to_use)
 
 # Find the maximum length of the DataFrames in train_patients_mixed
-max_length = max(len(df) for df in train_patients_mixed)
+#max_length = max(len(df) for df in train_patients_mixed)
 
 # Adjust the length of each DataFrame in X_train to match the maximum length and forward-fill missing values
-X_train: list[pd.DataFrame] = []
-y_train: list[pd.Series] = []
+# X_train = []
+# y_train = []
 
-for j in tqdm(range(len(train_patients_mixed)), "Extending indices and splitting into (X_train, y_train)"):
-    df = train_patients_mixed[j]
-    # Generate a new index that extends to the maximum length
-    new_index = pd.timedelta_range(start=df.index[0], periods=max_length, freq='h')
-    df = df.reindex(new_index).ffill()  # Reindex to the new index and forward-fill
-    X_train.append(df.drop(columns="SepsisLabel", inplace=False))
-    y_train.append(df["SepsisLabel"])
+# for j in tqdm(range(len(train_patients_mixed)), "Extending indices and splitting into (X_train, y_train)"):
+#     train_df = train_patients_mixed[j]
+#     # Generate a new index that extends to the maximum length
+#     new_index = pd.timedelta_range(start=train_df.index[0], periods=max_length, freq='h')
+#     train_df = train_df.reindex(new_index).ffill()  # Reindex to the new index and forward-fill
+#     X_train.append(train_df.drop(columns="SepsisLabel", inplace=False))
+#     y_train.append(train_df["SepsisLabel"])
 
-# Adjust the length of each DataFrame in X_test similarly
-X_test: list[pd.DataFrame] = []
-y_test: list[pd.Series] = []
+# # Adjust the length of each DataFrame in X_test similarly
+# X_test = []
+# y_test = []
 
-for k in tqdm(range(len(test_patients_mixed)), "Extending indices and splitting into (X_test, y_test)"):
-    df = test_patients_mixed[k]
-    new_index = pd.timedelta_range(start=df.index[0], periods=max_length, freq='h')
-    df = df.reindex(new_index).ffill()
-    X_test.append(df.drop(columns="SepsisLabel", inplace=False))
-    y_test.append(df["SepsisLabel"])
+# for k in tqdm(range(len(test_patients_mixed)), "Extending indices and splitting into (X_test, y_test)"):
+#     test_df = test_patients_mixed[k]
+#     new_index = pd.timedelta_range(start=test_df.index[0], periods=max_length, freq='h')
+#     test_df = test_df.reindex(new_index).ffill()
+#     X_test.append(test_df.drop(columns="SepsisLabel", inplace=False))
+#     y_test.append(test_df["SepsisLabel"])
+
+train = pd.concat(train_patients_mixed)
+test = pd.concat(test_patients_mixed)
+
+X_train = train.drop(columns="SepsisLabel", inplace=False)
+y_train = train["SepsisLabel"]
+X_test = test.drop(columns="SepsisLabel", inplace=False)
+y_test = test["SepsisLabel"]
 
 f = make_scorer(fbeta_score, beta=1)
 
-clf = xgb.XGBClassifier(objective="binary:logistic", eval_metric=f, scale_pos_weight=weight)
+clf = xgb.XGBClassifier(objective="binary:logistic", eval_metric=f, scale_pos_weight=ratio)
 bst = clf.fit(X_train, y_train)
 y_pred = bst.predict(X_test)
 
