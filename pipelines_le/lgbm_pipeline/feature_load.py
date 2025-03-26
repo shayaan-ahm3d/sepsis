873d2ffda3ed031b46c4532a48becaaca6d8acce
Shayaan Ahmed <117999566+shayaan-ahm3d@users.noreply.github.com>
2025-03-26 04:20:54 +0000

    filepath regex includes both set A & B, type annotations

--- pipelines_le/lgbm_pipeline/feature_load.py
+++ pipelines_le/lgbm_pipeline/feature_load.py
@@ -3,21 +3,21 @@ import glob
 import pandas as pd
 from tqdm import tqdm
 
-def load_training_data(path_pattern='../training_setA/*.psv', max_files=None):
+
+def load_training_data(path_pattern='../../training_set?/*.psv', max_files=None) -> list[pd.DataFrame]:
   """
-  Loads .psv files, removes the 6 columns before the sepsis label,
-  and concatenates into a single DataFrame.
+  Loads .psv files. Removes `Unit1`, `Unit2`, `HospAdmTime` & `ICULOS` columns before the `SepsisLabel`
   """
       
-  psv_files = glob.glob(path_pattern)
+  psv_files: list[str] = glob.glob(path_pattern)
     
   if max_files is not None:
       psv_files = psv_files[:max_files]
 
-  patients = []
+  patients: list[pd.DataFrame] = []
 
   for file_path in tqdm(psv_files, desc='Loading PSV Files'):
-      patient_record = pd.read_csv(file_path, sep='|')
+      patient_record: pd.DataFrame = pd.read_csv(file_path, sep='|')
 
     # Remove Unit1, Unit2, HospAdmTime, ICULOS columns
       patient_record = patient_record.drop(patient_record.columns[-5:-1], axis=1)
